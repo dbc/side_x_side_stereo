fdf6592b87904246dae8ba19bb962decbf38d87b
Dave Curtis <dave.n6nz@gmail.com>
2019-02-12 17:49:21 -0800

    Added MIT license and attribution of original public domain code.

--- src/side_by_side.cpp
+++ src/side_by_side.cpp
@@ -1,3 +1,39 @@
+// (MIT License)
+//
+// Copyright 2019 David B. Curtis
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////
+//
+// Subscribes to an image stream of side-by-side stereo 
+// where each image message consists of a left and right
+// image concatenated to form a single double-wide image. 
+// This node splits the incoming image down the middle
+// and republishes each half as stereo/left and stereo/right
+// images.
+//
+// Modified version of public domain code posted by 
+// PeteBlackerThe3rd in response to my question on ROS Answers:
+// https://answers.ros.org/question/315298/splitting-side-by-side-video-into-stereoleft-stereoright/
+// -- dbc
+
 #include <ros/ros.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
